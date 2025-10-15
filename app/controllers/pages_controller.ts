20ed6a7869ed386c0cd79d76214669b6037d1fb1
unknown <bimaekaputra@satyaterrabhinneka.ac.id>
2025-10-15 09:04:53 +0700

    initial commit HC system

---
+++ app/controllers/pages_controller.ts
@@ -0,0 +1,25 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class PagesController {
+
+    async Pegawai({ view }: HttpContext) {
+        return view.render('dashboard/pegawai')
+    }
+
+    async PegawaiDetail({ view }: HttpContext) {
+        return view.render('dashboard/pegawai_detail')
+    }
+
+    async Cuti({ view }: HttpContext) {
+        return view.render('dashboard/cuti')
+    }
+
+    async Sakit({ view }: HttpContext) {
+        return view.render('dashboard/sakit')
+    }
+
+    async Izin({ view }: HttpContext) {
+        return view.render('dashboard/izin')
+    }
+
+}
---
+++ start/routes.ts
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+const PagesController = () => import('#controllers/pages_controller')
+
+router.get('/', async ({ view }) => {
+    return view.render('layouts/dashboard')
+})
+
+router
+    .group(() => {
+        router.get('/pegawai', [PagesController, 'Pegawai'])
+        router.get('/pegawai-detail', [PagesController, 'PegawaiDetail'])
+        router.get('/cuti', [PagesController, 'Cuti'])
+        router.get('/sakit', [PagesController, 'Sakit'])
+        router.get('/izin', [PagesController, 'Izin'])
+    })
+    .prefix('halaman')
+
