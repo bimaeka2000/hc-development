7b9fecae74c2fa47be9fbf5de895fb6ec14cb67a
Jasswant Anbumani <blastress05@gmail.com>
2025-10-17 09:35:34 +0700

    "Added Data Looping To Pegawai, And Fixed Pegawai-Detail Layout Bih Ahh Nga"

--- app/models/jenis_cuti.ts
+++ app/models/jenis_cuti.ts
@@ -2,8 +2,12 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class JenisCuti extends BaseModel {
+  static table = 'jenis_cuti'
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare jenis_dokumen: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
